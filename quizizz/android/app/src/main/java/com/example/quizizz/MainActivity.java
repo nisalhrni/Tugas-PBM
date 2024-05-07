9ea19f8e2e6c5f87b3b71d78c9d6123eddf840e5
Anisa Al Harani <142313673+nisalhrni@users.noreply.github.com>
2024-05-07 17:43:50 +0700

    Delete quizizz directory

--- quizizz/android/app/src/main/java/com/example/quizizz/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.quizizz;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- quizizz/lib/question.dart
+++
@@ -1,6 +0,0 @@
-class Question {
-  String questionText;
-  bool questionAnswer;
-
-  Question({required this.questionText, required this.questionAnswer});
-}
